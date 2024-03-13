1e71651a85be03285f4240fa17db3d2ca0cea5c0
hhghss <160021473+hhghss@users.noreply.github.com>
2024-03-13 09:16:24 +0000

    Python use dictionary to add data in website

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
 from flask import Flask, render_template
-app = Flask(__name__)
 
-@app.route("/") 
+app = Flask(__name__)
+projects = [{
+    'id':
+    1,
+    'project':
+    'Web Scrapping',
+    'skills':
+    'urllib python, Python Requests, Selenium, Beautiful Soup'
+}, {
+    'id':
+    2,
+    'project':
+    'AI model for flare mointoring',
+    'skills':
+    'Python pandas, sikitlib, matblotlib,microsoft azure, pychatgpt'
+}, {
+    'id':
+    3,
+    'project':
+    'Web Scrapping',
+    'skills':
+    'urllib python, Python Requests, Selenium, Beautiful Soup'
+}]
+
+
+@app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',
+                         myskills=projects,
+                         myname='Hadeel Alghassab')
 
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=True) #Run Flask server
+  app.run(host='0.0.0.0', port=True)  #Run Flask server
